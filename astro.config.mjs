49191f6e037b05238f2c2c227b09a4e9c0bad443
Masna Bin Umeed <masnabinumeed@gmail.com>
2025-03-10 00:19:50 +0500

    Updated theming and styling
    Updated intro page content
    Updated favicon
    Updated logo
    Updated tailwind config theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,28 +5,36 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nizam.codeforpakistan.org',
-	integrations: [
-		starlight({
-			title: 'Digital Design Nizam',
-			social: {
-				github: 'https://github.com/codeforpakistan/design-system',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/getting-started' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  site: 'https://nizam.codeforpakistan.org',
+
+  integrations: [
+    tailwind(),
+    starlight({
+      title: 'Digital Design Nizam',
+      logo: {
+        light: './src/assets/light-logo.svg',
+        dark: './src/assets/dark-logo.svg',
+      },
+      social: {
+        github: 'https://github.com/codeforpakistan/design-system',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/getting-started' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+      editLink: {
+        baseUrl: 'https://github.com/codeforpakistan/design-system/edit/main', 
+      },
+      customCss: ['./src/tailwind.css'],
+    }),
+  ],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,23 @@
 import colors from 'tailwindcss/colors';
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
+
+
+const accent = { 50: '#E5F7EE', 100: '#B6E7CB', 200: '#80D5A3', 300: '#55C885', 400: '#2BBA67', 500: '#00AC48', 600: '#008F3C', 700: '#007330', 800: '#005126', 900: '#01411C',  960: '#002812' };
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		
 		extend: {
 			colors: {
 				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.emerald,
+				accent,
 				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
+				gray: colors.emerald,
+
 			},
 			fontFamily: {
 				// Your preferred text font. Starlight uses a system font stack by default.
